25e3b3f4be81bd0658ad8a3b32f66043f5a6afa8
Aditya Ramesh <ramesh.aditya@gmail.com>
2019-09-01 12:45:25 +0530

    Remove GUI hardcoded limit, PEP 8 formatting

--- app.py
+++ app.py
@@ -1,42 +1,56 @@
+"""
+Flask server to run Scoper with a GUI
+
+"""
+
 from flask import Flask, render_template, request
-from scoper import Scoper
 import nltk
+from scoper import Scoper
 
 nltk.download("brown")
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
 print("Please wait loading instance.....")
 FINDER = Scoper()
 
-@app.route("/",methods=["GET","POST"])
+
+@APP.route("/", methods=["GET", "POST"])
 def index():
     """
-        POST
+    Home page of Scoper. 
+
     """
-    if request.method == 'POST':
-        youtube_link = request.form['youtube_link']
-        algorithm = request.form['algorithm']
-        limit = request.form['limit']
-        query = request.form['query']
+    if request.method == "POST":
+        youtube_link = request.form["youtube_link"]
+        algorithm = request.form["algorithm"]
+        limit = request.form["limit"]
+        query = request.form["query"]
         print(query)
-        
+
         # check if the values are present
         if not (youtube_link and algorithm and limit):
-            return render_template("index.html", error="please enter proper values in the form", empty=True)
-                
+            return render_template(
+                "index.html", error="please enter proper values in the form", empty=True
+            )
+
         # get the query results
-        results = FINDER.main(youtube_link, query=query, limit=5, languages=["en"], mode=algorithm)
+        results = FINDER.main(
+            youtube_link,
+            query=query,
+            limit=int(limit),
+            languages=["en"],
+            mode=algorithm,
+        )
 
         # converting results from [('apple watch.', '29m 56s')] to [['apple watch.', '29m56s']]
-        results = [[x[0],x[1].replace(" ","")] for x in results]
-        
+        results = [[x[0], x[1].replace(" ", "")] for x in results]
+
         print(results)
         return render_template("index.html", videolink=youtube_link, results=results)
 
-    """
-        GET
-    """
     return render_template("index.html", empty=True)
+
+
 if __name__ == "__main__":
-    app.run()
+    APP.run()
